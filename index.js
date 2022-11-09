394b42401e90b00b2c56ebb8762df916d52eb31e
HollysKim <hollyskim521@gmail.com>
2022-11-08 20:22:36 -0500

    Able to draw, render a new grid of inputted number up to 100 and erase the grid

--- index.js
+++ index.js
@@ -1,9 +1,80 @@
-const page = document.querySelector('.container');
-const gridElements = document.querySelectorAll('.gridDiv');
+const page = document.querySelector('div');
+const gridBtn = document.querySelector('#gridBtn');
+const eraseBtn = document.querySelector('#eraseBtn');
 
-for(let i = 1; i < 257; i++) {
-    const div = document.createElement('div');
-    div.classList.add('.gridDiv');
-    page.appendChild(div);
+createGrid(16);
+
+function createGrid(val) {
+    page.style.gridTemplateColumns = `repeat(${val}, ${val}fr)`;
+    let loopVal = (val*val)+1;
+    for(let i = 1; i < loopVal; i++) {
+        const div = document.createElement('div');
+        div.classList.add('gridDiv');
+        page.appendChild(div);
+    };
+    let gridElements = document.querySelectorAll('.gridDiv');
+    mouseDown(gridElements);
+    eraseColor(gridElements);
 }
+
+function deleteGrid() {
+    document.querySelectorAll('.gridDiv').forEach((e) => e.parentNode.removeChild(e));
+}
+let colorDisabled = false;
+
+function mouseDown(gridElements) {
+    gridElements.forEach((div) => {
+        div.addEventListener('mousedown', () => {
+            div.classList.add('gridDivColorOn');
+            colorDisabled = false;
+            console.log('Mouse pressed down!');
+            mouseMove(gridElements);
+            mouseUp(gridElements);
+        })
+    })
+}
+
+function mouseMove(gridElements) {
+    gridElements.forEach((div) => {
+        div.addEventListener('mousemove', () => {
+            if(colorDisabled == true) {
+        
+            }
+            else {
+                div.classList.add('gridDivColorOn');
+                console.log('Mouse moved!');
+            }
+        })
+    })
+}
+
+function mouseUp(gridElements) {
+    gridElements.forEach((div) => {
+        div.addEventListener('mouseup', ()=> {
+            colorDisabled = true;
+            console.log('Mouse lifted up!');
+        })
+    })
+}
+
+
+    gridBtn.addEventListener('click', () => {
+        let gridSize = prompt('Input a grid size up to 100:');
+        if(gridSize > 100) {
+            alert('Inputs over 100 are not accepted.');
+        }
+        //page.style.gridTemplateColumns = 'repeat(1, 1fr)';
+        deleteGrid();
+        createGrid(gridSize);
+    });
+
+function eraseColor(gridElements) {
+    eraseBtn.addEventListener('click', ()=> {
+        gridElements.forEach((div) => {
+            div.classList.remove('gridDivColorOn');
+        })
+    })
+}
+
+
 
